f125a1627144b9dab6542fb500db0ef40ad2897f
ByFlowt <153213659+ByFlowt@users.noreply.github.com>
2025-10-17 16:45:08 +0200

    Deploy web app with secure API configuration

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "8cf74586748e7ed1d83314fa557a7e49",
+"flutter_bootstrap.js": "7abbfaa2ea8f436630b55d5742ab525c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5dd8ebca043152eea77727cf46f463c6",
-"/": "5dd8ebca043152eea77727cf46f463c6",
-"main.dart.js": "18046d133fe2e32d91365ee29ed31b7f",
+"index.html": "2a9848a55c4e4386dcda1feef3d24389",
+"/": "2a9848a55c4e4386dcda1feef3d24389",
+"main.dart.js": "437c6467b6fd8a0615e2d7325d66e334",
 "manifest.json": "4137147246a11ba2148318bc696c4202",
 "version.json": "408911c700dfebc786b3067677f71bc3"};
 // The application shell files that are downloaded before a service worker can
